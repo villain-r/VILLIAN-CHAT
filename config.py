6cc0a49540366e29c06e75f536cd4466bff223dd
brandedxkings <amritkaneki2627@gmail.com>
2024-11-17 21:43:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "176360aef3e85805ea17d304adf63f18"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "8081953831"))
-SUPPORT_GRP = "LOYAL_KINGDOM"
-UPDATE_CHNL = "LOYAL_IMPERIAL"
-OWNER_USERNAME = "ll_AMRIT_ll"
+OWNER_ID = int(getenv("OWNER_ID", "6474552945"))
+SUPPORT_GRP = "+nJZBaGgAUz84NDQ0"
+UPDATE_CHNL = "Dpz_V"
+OWNER_USERNAME = "ll_VIBER_ll"
